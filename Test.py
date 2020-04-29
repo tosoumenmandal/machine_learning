0e640626809c84508682f902ae55d55c4507ce23
tosoumenmandal <62945028+tosoumenmandal@users.noreply.github.com>
2020-04-29 12:06:29 +0530

    Delete Test.py

--- Test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from operator import itemgetter
-from sklearn.metrics import roc_auc_score
-from sklearn import linear_model
-
-print("Welcome to Github")
