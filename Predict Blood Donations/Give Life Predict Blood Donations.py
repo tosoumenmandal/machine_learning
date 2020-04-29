5707fe4765ec4500f3c2a4667c1103cacf1166ea
tosoumenmandal <62945028+tosoumenmandal@users.noreply.github.com>
2020-04-29 17:32:30 +0530

    Find Movie Similarity from Plot Summaries Changes Commited

--- Predict Blood Donations/Give Life Predict Blood Donations.py
+++ Predict Blood Donations/Give Life Predict Blood Donations.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 19 08:30:53 2020
-
-@author: Soumen
-"""
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
